3d061b94f37ca3c48e1c998f3d09b55dfe130110
Raveen K Sharma <raveen08@gmail.com>
2021-01-09 23:01:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const peeps = ["Lisa", "Kaitlin", "Jan"]
 
-function writeCards(peeps) {
+function writeCards(peeps, event) {
   for (let i =0; i < peeps.length; i++)
   {console.log(`Thank you, ${peeps[i]}, for the wonderful surprise gift!`);}
   
