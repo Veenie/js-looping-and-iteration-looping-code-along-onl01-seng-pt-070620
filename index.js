ff931883ae1b9079abea96a2454cb96ed0438b28
Raveen K Sharma <raveen08@gmail.com>
2021-01-09 23:12:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ function writeCards(peeps, event) {
   
 }
 
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
