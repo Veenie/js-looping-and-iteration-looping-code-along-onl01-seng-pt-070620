ee4e2915c9ea73876e0cead86c2ba39607b3d5e1
Raveen K Sharma <raveen08@gmail.com>
2021-01-09 22:55:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const peeps = ["Ada", "Brendan", "Ali"]
 function writeCards(peeps) {
   for (let i =0; i < peeps.length; i++)
   {console.log(`Thank you, ${peeps[i]}, for the wonderful birthday gift!`);}
+  return peeps;
 }
 
 writeCards(peeps);
